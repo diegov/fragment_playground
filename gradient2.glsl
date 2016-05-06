e405cb54a7a36170f8f56f17aeaedfaece0f2f2a
Diego Veralli <diegoov@gmail.com>
2016-05-06 08:08:25 +0200

    Updated examples

--- gradient2.glsl
+++ gradient2.glsl
@@ -18,19 +18,20 @@ void main() {
     float b = clamp(y, 0.0, 1.0);
     vec3 color1 = vec3(r, g, b) * light;
 
-    float rate = x * M_PI * 20.0;
-
-    x = pow(abs(x), length(vec2(x, y)) + 0.5);
-    y = pow(abs(y), length(vec2(x, y)) + 0.5);
+    x = pow(abs(x), length(vec2(x, y)) + 0.5) / 1.1;
+    y = pow(abs(y), length(vec2(x, y)) + 0.5) / 1.1;
 
     float min_repeat = 0.01;
 
-    float repeat1 = sin(time * M_PI / (5.0 + length(vec2(x, y)) / 3.0)) / (1.0 + min_repeat) + min_repeat;
-    float repeat2 = sin(time * M_PI / (16.0 + length(vec2(x, y)))) / (1.0 + min_repeat) + min_repeat;
+    float repeat1 = sin(time * M_PI / (11.0 + length(vec2(x, y)) / 3.0)) / (1.0 + min_repeat) + min_repeat;
+    float repeat2 = sin(time * M_PI / (19.0 + length(vec2(x, y)))) / (1.0 + min_repeat) + min_repeat;
     x = mod(x + repeat1 / 2.0, repeat1) - repeat1 / 2.0;
     y = mod(x + y + repeat2 / 2.0, repeat2) - repeat2 / 2.0;
 
+    float rate = x * M_PI * 20.0 + cos(y * M_PI * 10);
     vec3 other_color = vec3(sin(rate) / 2.0 + 0.5, 0.0, cos(rate) / 2.0 + 0.5);
 
-    color = mix(color1, other_color, pow(cos(y * M_PI * 10.0) / 2.0 + 0.5, 1.5));
+    float mix_factor = pow(cos(y * M_PI * 10.0) / 2.0 + 0.5, 1.5);
+    //float mix_factor = clamp(pow(abs(y) + 0.1, 1.5), 0.0, 1.0);
+    color = mix(color1, other_color, mix_factor);
 }
